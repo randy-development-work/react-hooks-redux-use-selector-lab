1a5b3c04f8aa7b52d0f7d83787e1254da661a80c
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-06-05 10:20:59 +0300

    List usernames and user count to pass tests.

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,14 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
 // add any needed imports here
 
 function Users() {
+  const users = useSelector((state) => state.users);
+  const userCount = useSelector((state)=> state.users.length)
+
+  let usernames = users.map((user) => {
+    return (
+      <li key = {user.username}>{user.username}</li>
+    )
+  })
   return (
     <div>
       <ul>
         Users!
         {/* Write code here that displays the usernames of all users in the Redux store */}
+        {usernames}
         {/* In addition, display the total number of users curently in the store */}
+
       </ul>
+        Total Users: {users.length}
+        {/* Total Users: {userCount} */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 
 import usersReducer from "./features/users/usersSlice";
 
 // add imports and code
+const store = configureStore({
+  reducer: usersReducer,
+});
 
 ReactDOM.render(
-  // add imports and code
-  <App />,
-  // add imports and code
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
   document.getElementById("root")
 );
